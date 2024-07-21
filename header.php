b14a7450feda511088c1cb54cff1901544fb925a
jaytality <johnathan.tiong@gmail.com>
2024-07-22 08:12:48 +1000

    split header into its own file

---
+++ header.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Header for the wp-jtiong theme
+ *
+ * displays everything in <head>
+ *
+ * @package wp-jtiong
+ * @since 1.0.0
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jtiong.blog</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link href="/public/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,600;0,900;1,100;1,400;1,600;1,900&display=swap" rel="stylesheet">
+
+    <link href="/public/css/style.css?<?=uniqid()?>" rel="stylesheet">
+    <link href="/public/css/datatables.css?<?=uniqid()?>" rel="stylesheet">
+</head>
