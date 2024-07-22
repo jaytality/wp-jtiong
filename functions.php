fddeff23e251c3d161825a34ca2277fae27321d9
jaytality <johnathan.tiong@gmail.com>
2024-07-22 11:07:02 +1000

    fixed style enqueueing

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function theme_styles() {
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style('bootstrap-style', get_theme_file_uri('assets/css/bootstrap.min.css'));   // my additional customisations to bootstrap 4.6x
+    wp_enqueue_style('style', get_theme_file_uri('assets/css/style.css'));
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
